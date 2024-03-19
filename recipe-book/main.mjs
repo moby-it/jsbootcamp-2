a51115c32b7571b8d6ccbb76eeee143743ba1c81
George Spanos <gspanos@moby-it.com>
2024-03-19 19:04:52 +0200

    backend: server static file via node http server

--- recipe-book/main.mjs
+++ recipe-book/main.mjs
@@ -1,1 +1,19 @@
-console.log('hello from main')
+import * as http from "node:http";
+import * as path from "node:path";
+import * as fs from 'node:fs';
+
+const PORT = 8000;
+
+const STATIC_PATH = path.join(process.cwd(), "./static");
+
+http.createServer(async (req, res) => {
+  if (req.url.includes('/api')) {
+    res.write('I an not the index html file');
+    res.end();
+  } else {
+    const path = req.url === '/' ? '/index.html' : req.url;
+    const file = await fs.promises.readFile(`${STATIC_PATH}${path}`);
+    res.write(file);
+    res.end();
+  }
+}).listen(PORT);
