3e13317eaf0621ec33c0c012ee6d5e1940a1a4d8
Sfa <alexandrossfak000@gmail.com>
2024-03-22 23:31:05 +0200

    Note space functionality

--- todo/main.js
+++ todo/main.js
@@ -1,5 +1,22 @@
-let no_notes_text = document.getElementById("emptynote");
+let number_of_notes = 0;
+const spaceOfNotes = document.createElement('span');
+const input = document.getElementById("inputbox");
 
 function add_note() {
-    document.getElementById("emptynote").remove();
+    if(number_of_notes === 0) { 
+        document.getElementById("emptynote").remove();
+        document.getElementById('mainframe').appendChild(spaceOfNotes);
+    }
+    let listedNote = document.createElement('section');
+    listedNote.style.width = "400px";
+    listedNote.style.height = "100px";
+    listedNote.style.backgroundColor = "hsl(53, 18%, 70%)";
+    listedNote.style.marginBottom = "16px";
+    listedNote.style.borderRadius= "14px";
+    spaceOfNotes.appendChild(listedNote);
+    
+    
+    
+    
+    number_of_notes += 1;
 }
