83840f34bd969033a53cda1b2744bf0d215bb4b4
Sfa <alexandrossfak000@gmail.com>
2024-03-25 21:19:29 +0200

    input bug fix

--- todo/main.js
+++ todo/main.js
@@ -1,19 +1,17 @@
 let number_of_notes = 0;
-const spaceOfNotes = document.createElement('span');
 const input = document.getElementById("inputbox");
 
 function add_note() {
     if(number_of_notes === 0) { 
         document.getElementById("emptynote").remove();
-        document.getElementById('mainframe').appendChild(spaceOfNotes);
     }
     let listedNote = document.createElement('section');
-    listedNote.style.width = "400px";
-    listedNote.style.height = "100px";
+    listedNote.style.width = "430px";
+    listedNote.style.height = "96px";
     listedNote.style.backgroundColor = "hsl(53, 18%, 70%)";
-    listedNote.style.marginBottom = "16px";
-    listedNote.style.borderRadius= "14px";
-    spaceOfNotes.appendChild(listedNote);
+    listedNote.style.marginBottom = "25px";
+    listedNote.style.borderRadius= "12px";
+    document.getElementById('mainframe').appendChild(listedNote);
     
     
     
