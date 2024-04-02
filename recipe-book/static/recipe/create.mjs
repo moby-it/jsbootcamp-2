8f58bc9b66c174026c359eeecc5c493a6ec87add
George Spanos <gspanos@moby-it.com>
2024-04-02 19:33:45 +0300

    add ingredient button

--- recipe-book/static/recipe/create.mjs
+++ recipe-book/static/recipe/create.mjs
@@ -1,4 +1,7 @@
 const submitButton = document.querySelector('.submit');
+const addButton = document.getElementById('add-button');
+
+addButton.addEventListener('click', addIngredient);
 
 function submit() {
 
@@ -15,8 +18,9 @@ function submit() {
     description: getDescription(),
     ingredients
   };
+  // should send call to the backend
   console.log(formValue);
-}
+};
 
 submitButton.addEventListener('click', (event) => {
   event.preventDefault();
@@ -42,4 +46,44 @@ function getQuantities() {
   const inputs = document.querySelectorAll('[name=quantity]');
   const quantities = Array.from(inputs).map(i => i.value);
   return quantities;
+}
+
+function addIngredient() {
+  console.log('should add ingredient');
+  const ingredientSection = document.createElement('section');
+  ingredientSection.className = 'ingredient';
+  const quantitySection = document.createElement('section');
+  let label = document.createElement('label');
+  label.setAttribute('for', 'quantity');
+  label.setAttribute('type', 'number');
+  let input = document.createElement('input');
+  input.id = 'quantity';
+  input.setAttribute('name', 'quantity');
+  input.setAttribute('type', 'number');
+  quantitySection.appendChild(label);
+  quantitySection.appendChild(input);
+  ingredientSection.appendChild(quantitySection);
+
+  const ingredientNameSection = document.createElement('section');
+  label = document.createElement('label');
+  label.setAttribute('for', 'Name');
+  label.setAttribute('type', 'text');
+  input = document.createElement('input');
+  input.id = 'Name';
+  input.setAttribute('name', 'ingredient-name');
+  input.setAttribute('type', 'text');
+  ingredientNameSection.appendChild(label);
+  ingredientNameSection.appendChild(input);
+
+  const button = document.createElement('button');
+  button.setAttribute('type', 'button');
+  button.classList.add('button', 'button-small', 'button-warn');
+  button.textContent = '-';
+  ingredientSection.appendChild(ingredientNameSection);
+  ingredientSection.appendChild(button);
+  const ingredientList = document.querySelector('.ingredient-list');
+  ingredientList.appendChild(ingredientSection);
+}
+function removeIngredient() {
+  console.log('should remove ingredient');
 }
