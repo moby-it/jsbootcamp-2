3cf3d01286fc3fe7439160f625d0f4ce98b7b746
George Spanos <gspanos@moby-it.com>
2024-05-28 17:27:03 +0300

    computed example with css classes

--- browser/counter.mjs
+++ browser/counter.mjs
@@ -3,10 +3,13 @@ createApp({
   setup() {
     const count = ref(0);
     const doubleCount = computed(() => count.value * 2);
+    const countClass = computed(() => count.value > 10 ? 'highlighted' : '');
+    
     const inc = () => count.value++;
     return {
       count,
       doubleCount,
+      countClass,
       inc
     };
   }
