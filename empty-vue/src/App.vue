bde4039532c3a51e4e7470e54cdaaff6aee4cbd9
George Spanos <gspanos@moby-it.com>
2024-05-30 18:13:45 +0300

    playing with Vue

--- empty-vue/src/App.vue
+++ empty-vue/src/App.vue
@@ -1,19 +1,27 @@
 <script setup>
 import { computed, ref } from 'vue';
 const count = ref(0);
-const doubleCount = computed(() => count.value > 10 ? count.value * 2 : 0
-);
+const newCount = ref(0);
+const doubleCount = computed(() => count.value * 2);
+const message = ref("");
 
-
-
+async function updateCount() {
+  count.value++;
+  newCount.value = count.value + 1;
+  const c = newCount.value * 100;
+  message.value = "C value is" + c;
+}
 </script>
 
 <template>
   <main>
     <h1>Hello from Vue</h1>
     <p>{{ count }}</p>
+    <p>{{ newCount }}</p>
     <p>{{ doubleCount }}</p>
-    <button @click="count = count + 1">Inc</button>
+    <p v-if="doubleCount > 20">Double count is over 20</p>
+    <p>{{ message }}</p>
+    <button @click="updateCount">Inc</button>
   </main>
 </template>
 
--- empty-vue/src/main.js
+++ empty-vue/src/main.js
@@ -1,6 +1,15 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
+
+
+const obj = {
+  get name() {
+    console.log('side effect');
+    return 'George';
+  }
+};
+obj.name;
